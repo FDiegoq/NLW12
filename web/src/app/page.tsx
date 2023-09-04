427db6e220683e9c36186b47dc5bc8ec963d4523
Diego <119186100+FDiegoq@users.noreply.github.com>
2023-09-04 17:49:32 -0300

    pagina final

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden bg-[url(../assets/bg_stars.SVG)] bg-cover">
-         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 bg-purple-700 opacity-58 rounded-full blur-full">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative     overflow-hidden border-r border-white/10 bg-[url(../assets/bg_stars.SVG)] bg-cover">
+         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-58 rounded-full blur-full">
          </div>
 
          <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
@@ -29,13 +29,16 @@ export default function Home() {
             <h1 className="mt-5 text-5xl font-bold leading-tight text-white">Sua cápsula do tempo</h1>
             <p className="text-lg leading-relaxed">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
            </div>
+          
+          <a href="" className="inline-block rounded-full bg-green-500 px-5 py-3 text-black font-alt text-sm uppercase leading-none hover:text-gray-50 hover:bg-green-700 transition-colors">CADASTRAR LEMBRANÇA</a>
 
          </div>
 
-         <a href="" className="inline-block rounded-full bg-green-500 px-5 py-3 text-black font-bold hover:text-gray-50 hover:bg-green-700 transition-colors">CADASTRAR LEMBRANÇA</a>
 
 
-
+         <div className="text-sm leading-relaxed text-gray-200">
+          <p>Feito com 💜 no NLW da <a target="_blank" href="https://rocketseat.com.br" className="underline hover:text-gray-100">Rocketseat</a></p>
+         </div>
       </div>
 
       <div className="flex flex-col p-16 bg-[url(../assets/bg_stars.SVG)] bg-cover">
